4bf9451aa361ecc5ba9317dc461f71932bea3d9b
kuruppa0616 <kuruppa0616@gmail.com>
2018-08-06 13:00:39 +0900

    reset

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MenberBox from './MenberBox';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-let  data=[
-	{id:1,author:"水冷さん"},
-	{id:2,author:"レオさん"}
-];
 
-ReactDOM.render(<MenberBox data ={data} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
